8ddf45b39a56c64a91bda9899cc3b5ef51d2c63a
Nambi.Rajan <nambi.rajan@cognizant.com>
2019-04-25 12:36:00 +0530

    docker file and env changes -2

--- MovieCruiserAuthenticationService/env-variable.sh
+++ MovieCruiserAuthenticationService/env-variable.sh
@@ -1,5 +1,5 @@
 export MYSQL_DATABASE=moviedb
-export MYSQL_USER=app_root
+export MYSQL_USER=root
 export MYSQL_PASSWORD=root
 if [[ -z "${MYSQL_CI_URL}" ]]; then
 export MYSQL_CI_URL=jdbc:mysql://localhost:3306/moviedb
